029ce72ee79749784306bd5a124460b3bc0cda44
pibupi <pibupi@sina.com>
2021-03-08 00:37:17 +0800

    init

---
+++ umi3_crud_tx/src/pages/About/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const index = () => {
+  return (
+    <div>
+      <h1>老师微信: 27732357</h1>
+      <p>购买课程: 加老师微信,提供课程单号,加入课程答疑微信群!</p>
+      <img width="1000" src={require('@/assets/order.png')} alt="" />
+    </div>
+  );
+};
+
+export default index;
